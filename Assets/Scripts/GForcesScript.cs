7325b7b66a0098d711c750330940bd7bd6a0c9df
Tim Shmidt <timsh007@gmail.com>
2025-11-22 15:21:34 -0800

    nevermind it didnt do that

--- Assets/Scripts/GForcesScript.cs
+++ Assets/Scripts/GForcesScript.cs
@@ -26,11 +26,22 @@ public class GForcesScript : MonoBehaviour {
     [SerializeField] private GameObject fire;
     [SerializeField] private GameObject explosion;
     private bool destroyed = false;
+
+    bool justRolledOver = false;
+    int counterPastRollover = 0;
     
     void FixedUpdate() {
         GetComponent<Animator>().SetInteger("yScale", (int) transform.localScale.y);
         if (feltGs < rollOverThresh && GetComponent<Rigidbody2D>().linearVelocity.magnitude > minRolloverSpeed && !GetComponent<PlaneController>().pilotDeadOrGone() && !sleepy && rolloverAllowingSprites.Contains(GetComponent<SpriteRenderer>().sprite)) {
             rollover();
+            justRolledOver = true;
+        }
+        if (justRolledOver) {
+            counterPastRollover++;
+        }
+        if (counterPastRollover == 10) {
+            justRolledOver = false;
+            counterPastRollover = 0;
         }
         if (overGPlaneToDeath() && !destroyed) {
             destroyed = true;
@@ -43,7 +54,7 @@ public class GForcesScript : MonoBehaviour {
                 dm.GetComponent<DamageModel>().kill();
             }
         }
-        if (overGPlane()) {
+        if (overGPlane() && !justRolledOver) {
             if (transform.Find("WingHitbox") != null) transform.Find("WingHitbox").GetComponent<DamageModel>().kill();
             //if (transform.Find("TailHitbox") != null) transform.Find("TailHitbox").GetComponent<DamageModel>().kill();
         }
