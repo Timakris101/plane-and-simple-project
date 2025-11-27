782a213839d95da8b3c045adb86d13193b59e29a
Tim Shmidt <timsh007@gmail.com>
2025-11-27 00:20:32 -0800

    made bullets have fragmentation

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 using static Utils;
 
 public class BulletScript : MonoBehaviour {
-    private static float explosiveRangeOfCertainHit = 5f;
-
     [Header("Base Stats")]
     [SerializeField] private float initSpeed;
     [SerializeField] private float damage;
     [SerializeField] private float explosionRad;
     [SerializeField] private float penetrationVal;
+    [SerializeField] private float fragmentationAmt;
+    [SerializeField] private float fragmentationStrength;
     [SerializeField] private float armorPenMeters;
-    [SerializeField] private float maxFlyPastDist;
+    [SerializeField] private float armorFuseSec;
     [SerializeField] private float armingDist;
     [SerializeField] private float fuseTimeSec;
     [SerializeField] private GameObject effect;
@@ -34,13 +34,94 @@ public class BulletScript : MonoBehaviour {
     }
 
     void dealDamage(Collision2D col) {
+        int armorHitCount;
+        float effectiveArmorPen;
+        float newPenVal;
+        GameObject objClosestToBullet;
+        handleArmor(col, out armorHitCount, out effectiveArmorPen, out newPenVal, out objClosestToBullet);
+
+        if (armorHitCount == 1 && effectiveArmorPen <= 0f && objClosestToBullet.GetComponent<ArmorScript>() != null) return;
+
+        if (objClosestToBullet == null && col.transform.GetComponent<BoxCollider2D>() != null) {
+            missObj(col);
+            return;
+        }  
+
+        if (didMiss(col, effectiveArmorPen, newPenVal) && col.transform.GetComponent<BoxCollider2D>() != null) {
+            missObj(col);
+            return;
+        }
+
+        handleDamage(col, armorFuseSec, effectiveArmorPen, newPenVal);
+
+        makeEffectAndDestroyObj(transform.position);
+    }
+
+    void dealDamage() {
+        float effectiveArmorPen = armorPenMeters * prevVel.magnitude / initSpeed;
+        Vector3 beginningHitPos = transform.position - prevVel * Time.fixedDeltaTime;
+        handleDamage(beginningHitPos, GetComponent<Rigidbody2D>().linearVelocity, armorFuseSec, effectiveArmorPen, penetrationVal);
+
+        makeEffectAndDestroyObj(transform.position);
+    }
+
+    bool didMiss(Collision2D col, float effectiveArmorPen, float newPenVal) {
+        Vector3 beginningHitPos = transform.position - prevVel * Time.fixedDeltaTime;
+        RaycastHit2D[] hits = Physics2D.RaycastAll(beginningHitPos - (Vector3) col.relativeVelocity.normalized, -col.relativeVelocity, penetrationVal);
+        foreach (RaycastHit2D hit in hits) {
+            if (hit.transform.gameObject != maxAncestor(col.gameObject)) continue;
+            if (hit.collider.transform.GetComponent<DamageModel>() != null) {
+                DamageModel d = hit.collider.transform.GetComponent<DamageModel>();
+                if (Random.Range(0f, 1f) > (d.getHitChance(Vector3.Angle(-col.relativeVelocity, d.transform.right)))) continue;
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    void missObj(Collision2D col) {
+        col.transform.GetComponent<Rigidbody2D>().AddForceAtPosition(-(prevVel - (Vector3) GetComponent<Rigidbody2D>().linearVelocity) * GetComponent<Rigidbody2D>().mass, transform.position, ForceMode2D.Impulse);
+        GetComponent<Rigidbody2D>().linearVelocity = prevVel;
+        Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), col.transform.GetComponent<Collider2D>());
+    }
+
+    void handleDamage(Collision2D col, float postHitFuse, float effectiveArmorPen, float newPenVal) {
+        handleDamage(col.contacts[0].point, -col.relativeVelocity, postHitFuse, effectiveArmorPen, newPenVal);
+    }
+
+    void handleDamage(Vector3 beginningHitPos, Vector3 relativeVel, float postHitFuse, float effectiveArmorPen, float newPenVal) {
+        for (int i = 0; i < fragmentationAmt; i++) {
+            Vector2 fragmentationVector = i == 0 ? new Vector2(0,0) : new Vector2(Random.Range(-fragmentationStrength, fragmentationStrength), Random.Range(-fragmentationStrength, fragmentationStrength));
+            Vector2 fragVecPlusVel = fragmentationVector + (Vector2) relativeVel;
+            RaycastHit2D[] hits = Physics2D.RaycastAll(beginningHitPos + relativeVel * postHitFuse, fragVecPlusVel, newPenVal);
+            Debug.DrawRay(beginningHitPos + relativeVel * postHitFuse, fragVecPlusVel.normalized * newPenVal, Color.red, 10f);
+            foreach (RaycastHit2D hit in hits) {
+                if (hit.collider.transform.GetComponent<DamageModel>() != null) {
+                    DamageModel d = hit.collider.transform.GetComponent<DamageModel>();
+                    if (!(Random.Range(0f, 1f) < (d.getHitChance(Vector3.Angle(fragVecPlusVel, d.transform.right))))) continue;
+                    d.damage(Random.Range((1f - damageVariation), (1f + damageVariation)) * damage);
+                }
+                if (hit.collider.transform.GetComponent<ArmorScript>() != null) {
+                    float effArmorThickness = Mathf.Abs(hit.collider.transform.GetComponent<BoxCollider2D>().size.x / Mathf.Cos(Mathf.Deg2Rad * Vector3.Angle(prevVel, hit.normal)));
+                    if (effArmorThickness < effectiveArmorPen) {
+                        effectiveArmorPen -= effArmorThickness;
+                    } else {
+                        effectiveArmorPen = 0f;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    void handleArmor(Collision2D col, out int armorHitCount, out float effectiveArmorPen, out float newPenVal, out GameObject objClosestToBullet) {
         Vector3 beginningHitPos = transform.position - prevVel * Time.fixedDeltaTime;
         RaycastHit2D[] hits = Physics2D.RaycastAll(beginningHitPos, -col.relativeVelocity);
-        float newPenVal = penetrationVal;
-        int armorHitCount = 0;
+        newPenVal = penetrationVal;
+        armorHitCount = 0;
         int index = 0;
-        float effectiveArmorPen = armorPenMeters * col.relativeVelocity.magnitude / initSpeed;
-        GameObject objClosestToBullet = null;
+        effectiveArmorPen = armorPenMeters * col.relativeVelocity.magnitude / initSpeed;
+        objClosestToBullet = null;
         foreach (RaycastHit2D hit in hits) {
             if (hit.transform == transform) continue;
             if (hit.transform.gameObject != maxAncestor(col.gameObject)) continue;
@@ -51,7 +132,7 @@ public class BulletScript : MonoBehaviour {
             }
 
             if (hit.collider.transform.GetComponent<ArmorScript>() != null) {
-                float effArmorThickness = hit.collider.transform.GetComponent<BoxCollider2D>().size.x / Mathf.Cos(Mathf.Deg2Rad * Vector3.Angle(col.relativeVelocity, hit.normal));
+                float effArmorThickness = Mathf.Abs(hit.collider.transform.GetComponent<BoxCollider2D>().size.x / Mathf.Cos(Mathf.Deg2Rad * Vector3.Angle(col.relativeVelocity, hit.normal)));
                 armorHitCount++;
                 if (effArmorThickness < effectiveArmorPen) {
                     effectiveArmorPen -= effArmorThickness;
@@ -63,46 +144,6 @@ public class BulletScript : MonoBehaviour {
             }
             index++;
         }
-        if (objClosestToBullet == null) {
-            if (col.transform.GetComponent<BoxCollider2D>() != null) {
-                col.transform.GetComponent<Rigidbody2D>().AddForceAtPosition(-(prevVel - (Vector3) GetComponent<Rigidbody2D>().linearVelocity) * GetComponent<Rigidbody2D>().mass, transform.position, ForceMode2D.Impulse);
-                GetComponent<Rigidbody2D>().linearVelocity = prevVel;
-                Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), col.transform.GetComponent<Collider2D>());
-                return;
-            }
-        }
-        if (explosionRad < explosiveRangeOfCertainHit && armorHitCount == 1 && effectiveArmorPen <= 0f && objClosestToBullet.GetComponent<ArmorScript>() != null) return;
-        hits = Physics2D.CircleCastAll(beginningHitPos - (Vector3) col.relativeVelocity.normalized * Random.Range(0f, maxFlyPastDist), explosionRad == 0 ? transform.localScale.x : explosionRad, -col.relativeVelocity, newPenVal);
-        int counter = 0;
-        foreach (RaycastHit2D hit in hits) {
-            if (explosionRad < explosiveRangeOfCertainHit && hit.transform.gameObject != maxAncestor(col.gameObject)) continue;
-            if (hit.collider.transform.GetComponent<DamageModel>() != null) {
-                DamageModel d = hit.collider.transform.GetComponent<DamageModel>();
-                if (!(Random.Range(0f, 1f) < (explosionRad < explosiveRangeOfCertainHit ? d.getHitChance(Vector3.Angle(col.relativeVelocity, d.transform.right)) : 1f))) continue;
-                counter++;
-                d.damage(Random.Range((1f - damageVariation) * damage, (1f + damageVariation) * damage));
-            }
-        }
-        if (counter == 0 && col.transform.GetComponent<BoxCollider2D>() != null) {
-            col.transform.GetComponent<Rigidbody2D>().AddForceAtPosition(-(prevVel - (Vector3) GetComponent<Rigidbody2D>().linearVelocity) * GetComponent<Rigidbody2D>().mass, transform.position, ForceMode2D.Impulse);
-            GetComponent<Rigidbody2D>().linearVelocity = prevVel;
-            Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), col.transform.GetComponent<Collider2D>());
-            return;
-        }
-        makeEffectAndDestroyObj(transform.position);
-    }
-
-    void dealDamage() {
-        Vector3 beginningHitPos = transform.position + (Vector3) GetComponent<Rigidbody2D>().linearVelocity.normalized * Random.Range(0f, maxFlyPastDist) - prevVel * Time.fixedDeltaTime;
-        RaycastHit2D[] hits = Physics2D.CircleCastAll(beginningHitPos, explosionRad == 0 ? transform.localScale.x : explosionRad, GetComponent<Rigidbody2D>().linearVelocity.normalized, penetrationVal);
-        foreach (RaycastHit2D hit in hits) {
-            if (hit.collider.transform.GetComponent<DamageModel>() != null) {
-                DamageModel d = hit.collider.transform.GetComponent<DamageModel>();
-                if (!(Random.Range(0f, 1f) < (explosionRad < explosiveRangeOfCertainHit ? d.getHitChance(Vector3.Angle(GetComponent<Rigidbody2D>().linearVelocity, d.transform.right)) : 1f))) continue;
-                d.damage(Random.Range((1f - damageVariation) * damage, (1f + damageVariation) * damage));
-            }
-        }
-        makeEffectAndDestroyObj(transform.position);
     }
 
     private void makeEffectAndDestroyObj(Vector3 effectPos) {
