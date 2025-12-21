2b94e3b9b5a3c29fffc75883367f907b905edd40
Tim Shmidt <timsh007@gmail.com>
2025-12-20 23:34:56 -0800

    added "brightness", very naive

--- Assets/Scripts/SkyScript.cs
+++ Assets/Scripts/SkyScript.cs
@@ -54,6 +54,10 @@ public class SkyScript : MonoBehaviour {
         }
 
         texture.Apply();
+
+        camera.transform.Find("Canvas").Find("Brightness").GetComponent<UnityEngine.UI.Image>().color = new Color(0f, 0f, 0f, Mathf.Max(0f, camera.transform.Find("Canvas").Find("GForceDisp").GetComponent<UnityEngine.UI.Image>().color.a - Time.deltaTime));
+        camera.transform.Find("Canvas").Find("Brightness").GetComponent<RectTransform>().sizeDelta = camera.transform.Find("Canvas").GetComponent<RectTransform>().sizeDelta;
+        camera.transform.Find("Canvas").Find("Brightness").GetComponent<UnityEngine.UI.Image>().color = new Color(0f, 0f, 0f, (.7f - (gradient.Evaluate(percentDay).maxColorComponent)));
     }
 // not working
     // private Vector3 lightSeenInViewDir(Vector3 dir, float stepSize) {
