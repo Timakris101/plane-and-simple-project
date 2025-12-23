22e3309b36cce63631720b9008706fbff5a3cd39
Tim Shmidt <timsh007@gmail.com>
2025-12-23 14:35:48 -0800

    added *funkier* terrain generation

--- Assets/Scripts/TerrainGen.cs
+++ Assets/Scripts/TerrainGen.cs
@@ -9,31 +9,65 @@ public class TerrainGen : MonoBehaviour {
     [SerializeField] private float terrainLength;
     [SerializeField] private float maxHeight;
     [SerializeField] private AnimationCurve baseTerrHeight;
+
+    [SerializeField] private bool funkyGen;
     void Awake() {
-        Vector2[] terrainVecs = new Vector2[(int) terrainPointAmt];
-        GetComponent<SpriteShapeController>().spline.Clear();
-        for (int i = 0; i < terrainPointAmt; i++) {
-            terrainVecs[i] = new Vector2(-(terrainLength / 2) + i * (terrainLength / terrainPointAmt), Random.Range(maxHeight / 2f, maxHeight) + baseTerrHeight.Evaluate(i / terrainPointAmt));
-            GetComponent<SpriteShapeController>().spline.InsertPointAt(i, new Vector2(terrainVecs[i].x, i == 0 || i == terrainPointAmt - 1 ? 0f : terrainVecs[i].y));
-        }
-        terrainVecs[0] = new Vector2(-terrainLength / 2, 0f);
+        if (funkyGen) {
+            Vector2[] terrainVecs = new Vector2[(int) terrainPointAmt];
+            GetComponent<SpriteShapeController>().spline.Clear();
+            float terrainStepSize = terrainLength / terrainPointAmt;
 
-        terrainVecs[(int) terrainPointAmt - 1] = new Vector2(terrainLength / 2, 0f);
+            for (int i = 0; i < terrainPointAmt; i++) {
+                if (i != 0) {
+                    int counter = 0;
+                    bool selfIntersects = false;
+                    terrainVecs[i] = terrainVecs[i - 1] + (new Vector2(Random.Range(-.5f, 1f), Random.Range(-2f, 1f))).normalized * 5 * terrainStepSize;
+                    if (terrainVecs[i].y < 0f) terrainVecs[i] = new Vector2(terrainVecs[i].x, -terrainVecs[i].y);
+                }
 
-        GetComponent<PolygonCollider2D>().SetPath(0, terrainVecs);
+                if (terrainVecs[i].x > terrainLength / 2) terrainVecs[i] = terrainVecs[i - 1] + new Vector2(.1f, 0);
 
-        GetComponent<SpriteShapeRenderer>().localBounds = new Bounds(Vector3.zero, new Vector3(terrainLength * transform.localScale.x, maxHeight * transform.localScale.y + maxBaseTerrHeight(0.01f), 0)); 
+                terrainVecs[0] = new Vector2(-terrainLength / 2, 0f);
 
-        if (transform.childCount != 0) {
-            transform.GetChild(0).GetComponent<SpriteRenderer>().size = new Vector2(terrainLength * 2f, Constants.Water.seaLevel * 2f);
-            transform.GetChild(0).GetComponent<BoxCollider2D>().size = new Vector2(terrainLength, Constants.Water.seaLevel * 2f);
+                terrainVecs[(int) terrainPointAmt - 1] = new Vector2(terrainLength / 2, 0f);
+
+                GetComponent<SpriteShapeController>().spline.InsertPointAt(i, new Vector2(terrainVecs[i].x, i == 0 || i == terrainPointAmt - 1 ? 0f : terrainVecs[i].y));
+            }
+
+            GetComponent<PolygonCollider2D>().SetPath(0, terrainVecs);
+
+            GetComponent<SpriteShapeRenderer>().localBounds = new Bounds(Vector3.zero, new Vector3(terrainLength * transform.localScale.x, transform.localScale.y * maxTerrHeight(terrainVecs), 0)); 
+
+            if (transform.childCount != 0) {
+                transform.GetChild(0).GetComponent<SpriteRenderer>().size = new Vector2(terrainLength * 2f, Constants.Water.seaLevel * 2f);
+                transform.GetChild(0).GetComponent<BoxCollider2D>().size = new Vector2(terrainLength, Constants.Water.seaLevel * 2f);
+            }
+        } else {
+            Vector2[] terrainVecs = new Vector2[(int) terrainPointAmt];
+            GetComponent<SpriteShapeController>().spline.Clear();
+            for (int i = 0; i < terrainPointAmt; i++) {
+                terrainVecs[i] = new Vector2(-(terrainLength / 2) + i * (terrainLength / terrainPointAmt), Random.Range(maxHeight / 2f, maxHeight) + baseTerrHeight.Evaluate(i / terrainPointAmt));
+                GetComponent<SpriteShapeController>().spline.InsertPointAt(i, new Vector2(terrainVecs[i].x, i == 0 || i == terrainPointAmt - 1 ? 0f : terrainVecs[i].y));
+            }
+            terrainVecs[0] = new Vector2(-terrainLength / 2, 0f);
+
+            terrainVecs[(int) terrainPointAmt - 1] = new Vector2(terrainLength / 2, 0f);
+
+            GetComponent<PolygonCollider2D>().SetPath(0, terrainVecs);
+
+            GetComponent<SpriteShapeRenderer>().localBounds = new Bounds(Vector3.zero, new Vector3(terrainLength * transform.localScale.x, maxHeight * transform.localScale.y + maxTerrHeight(terrainVecs), 0)); 
+
+            if (transform.childCount != 0) {
+                transform.GetChild(0).GetComponent<SpriteRenderer>().size = new Vector2(terrainLength * 2f, Constants.Water.seaLevel * 2f);
+                transform.GetChild(0).GetComponent<BoxCollider2D>().size = new Vector2(terrainLength, Constants.Water.seaLevel * 2f);
+            }
         }
     }
 
-    public float maxBaseTerrHeight(float precision) {
+    public float maxTerrHeight(Vector2[] terrainVecs) {
         float max = 0f;
-        for (float i = 0f; i <= 1f; i += precision) {
-            if (baseTerrHeight.Evaluate(i) > max) max = baseTerrHeight.Evaluate(i);
+        for (int i = 0; i < terrainPointAmt; i++) {
+            if (terrainVecs[i].y > max) max = terrainVecs[i].y;
         }
         return max;
     }
